e05d12b28912b96dbf3a8cdf98e9ef1db8f9f8d1
Benjamin Fischer <benjaminscoding@gmail.com>
2022-12-09 08:53:21 +1300

    cart .map warning resolved

--- client/src/redux/slices/cart.js
+++ client/src/redux/slices/cart.js
@@ -2,9 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const calculateSubtotal = (cartState) => {
   let result = 0;
-  cartState.map((item) => {
-    result += item.qty * item.price;
-  });
+  cartState.map((item) => (result += item.qty * item.price));
   return Number(result).toFixed(2);
 };
 
